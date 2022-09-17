836450b53935eef3d0554018520f8281d9217118
CieleckiJacek <93492850+Jackk83@users.noreply.github.com>
2022-09-17 22:01:30 +0200

    test update controller

--- WSBLearn.WebApi/Controllers/WeatherForecastController.cs
+++ WSBLearn.WebApi/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ namespace WSBLearn.WebApi.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Tested = true
             })
             .ToArray();
         }
--- WSBLearn.WebApi/WeatherForecast.cs
+++ WSBLearn.WebApi/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace WSBLearn.WebApi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public bool Tested { get; set; }
     }
 }
