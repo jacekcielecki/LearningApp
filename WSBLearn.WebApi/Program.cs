07b0258e9304d87616944e86393fbbfcfc63ac3e
CieleckiJacek <93492850+Jackk83@users.noreply.github.com>
2022-09-22 20:57:27 +0200

    remove azure dependencies

--- WSBLearn.WebApi/Program.cs
+++ WSBLearn.WebApi/Program.cs
@@ -1,17 +1,10 @@
-using Microsoft.Extensions.Logging.AzureAppServices;
-using System.Configuration;
 using WSBLearn.Application.Services;
-using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 builder.Services.AddScoped<IQuestionService, QuestionService>();
 
 
@@ -19,23 +12,6 @@ builder.Services.AddLogging();
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
-builder.Logging.AddAzureWebAppDiagnostics();
-builder.Services.Configure<AzureBlobLoggerOptions>(options =>
-{
-    options.BlobName = "log.txt";
-});
-builder.Services.Configure<AzureFileLoggerOptions>(options =>
-{
-    options.FileName = "azure-diagnostics-";
-    options.FileSizeLimit = 50 * 1024;
-    options.RetainedFileCountLimit = 5;
-});
-builder.Services.AddAzureClients(clientBuilder =>
-{
-    clientBuilder.AddBlobServiceClient(builder.Configuration["AzureStorageConnectionString:blob"], preferMsi: true);
-    clientBuilder.AddQueueServiceClient(builder.Configuration["AzureStorageConnectionString:queue"], preferMsi: true);
-});
-
 
 var app = builder.Build();
 
