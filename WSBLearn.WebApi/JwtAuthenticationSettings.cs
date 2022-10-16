bffb73a7e0399638c0fa73dfad68dfae92043a3f
CieleckiJacek <93492850+Jackk83@users.noreply.github.com>
2022-10-16 18:17:02 +0200

    Remove duplicat

--- WSBLearn.WebApi/JwtAuthenticationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WSBLearn.Application
-{
-    public class JwtAuthenticationSettings
-    {
-        public string JwtKey { get; set; }
-        public int JwtExpireDays { get; set; }
-        public string JwtIssuer { get; set; }
-    }
-}
