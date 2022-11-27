13acf141aac6e60d691118490b7f8a3af77d27d7
CieleckiJacek <93492850+Jackk83@users.noreply.github.com>
2022-11-27 22:09:55 +0100

    add random blob filename

---
+++ WSBLearn.Application/Extensions/FileValidation.cs
@@ -0,0 +1,12 @@
+﻿namespace WSBLearn.Application.Extensions
+{
+    public static class FileValidation
+    {
+        public static bool IsUnsupportedImageFileExtension(string fileName)
+        {
+            var ext = Path.GetExtension(fileName);
+            var list = new List<string> { ".jpg", ".png", ".svg" };
+            return !list.Contains(ext);
+        }
+    }
+}
