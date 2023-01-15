1ca717f076ae02b2206a92f7e153b2110fb54f9a
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-01-15 21:18:17 +0100

    Update Messages.cs remove CrudMessages

--- WSBLearn.Application/Constants/Messages.cs
+++ WSBLearn.Application/Constants/Messages.cs
@@ -6,12 +6,6 @@
         public const string GenericErrorMessage = "Something went wrong";
     }
 
-    public static class CrudMessages
-    {
-        public const string CreateEntitySuccess = "{0} has been successfully created. Id: {1}";
-        public const string DeleteEntitySuccess = "{0} has been successfully deleted";
-    }
-
     public static class Defaults
     {
         public const string ProfilePictureUrl = "https://wsblearnstorage.blob.core.windows.net/avatarcontainer/default_profilepic-3fc14e29-cce0-462a-8081-2a2399da74f2.png";
