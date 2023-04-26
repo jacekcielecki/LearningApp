c26c5fca5eef5b6bf7608743592492036f2e0385
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-04-26 20:31:21 +0200

    add Test projects

---
+++ LearningApp.WebApi.Tests/Controllers/AchievementControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace LearningApp.WebApi.Tests.Controllers
+{
+    public class AchievementControllerTests
+    {
+        [Fact]
+        public async Task GetAllAsync_WithExistingItems_ReturnsItems()
+        {
+            //arrange
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            //act
+            var response = await client.GetAsync("/api/Achievement");
+
+            //assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
