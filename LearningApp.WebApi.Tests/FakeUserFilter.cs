0619b780104edeb876270b6ea782dadabc783728
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-04-28 12:03:51 +0200

    add FakeUserFilter

---
+++ LearningApp.WebApi.Tests/FakeUserFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace LearningApp.WebApi.Tests
+{
+    public class FakeUserFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var claimsPrincipal = new ClaimsPrincipal();
+
+            claimsPrincipal.AddIdentity(new ClaimsIdentity(
+                new []
+                {
+                    new Claim(JwtRegisteredClaimNames.Jti, "1"),
+                    new Claim(JwtRegisteredClaimNames.GivenName, "Admin"),
+                    new Claim(JwtRegisteredClaimNames.Email, "admin@mail.com"),
+                    new Claim(ClaimTypes.Role, "Admin")
+                }));
+
+            context.HttpContext.User = claimsPrincipal;
+            await next();
+        }
+    }
+}
