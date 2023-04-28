3e833b7cd2d0e35545c5518c487511139752a97d
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-04-28 12:05:02 +0200

    Add HttpContentHelper

---
+++ LearningApp.WebApi.Tests/Helpers/HttpContentHelper.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace LearningApp.WebApi.Tests.Helpers
+{
+    public static class HttpContentHelper
+    {
+        public static HttpContent ToJsonHttpContent(this object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return httpContent;
+        }
+    }
+}
