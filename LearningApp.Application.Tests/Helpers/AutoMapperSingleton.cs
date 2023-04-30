71a7a5de7f91e6c34c34f8eb43e8538987cfe6b7
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-04-30 21:11:48 +0200

    Create LearningApp/Application.Tests Helpers, add AutoMapperSingleton

---
+++ LearningApp.Application.Tests/Helpers/AutoMapperSingleton.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using LearningApp.Application.Common;
+
+namespace LearningApp.Application.Tests.Helpers
+{
+    public class AutoMapperSingleton
+    {
+        private static IMapper? _mapper;
+        public static IMapper? Mapper
+        {
+            get
+            {
+                if (_mapper != null) return _mapper;
+                var mappingConfig = new MapperConfiguration(mc => { mc.AddProfile(new WsbLearnMappingProfile()); });
+                var mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+                return _mapper;
+            }
+        }
+    }
+}
