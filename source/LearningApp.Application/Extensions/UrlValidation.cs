0f42fe722f8e7952a1c24bddd67a89f0e37ad3fc
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-06-12 20:03:52 +0200

    Refactor UrlValidation

--- source/LearningApp.Application/Extensions/UrlValidation.cs
+++ source/LearningApp.Application/Extensions/UrlValidation.cs
@@ -4,13 +4,14 @@
     {
         public static bool UrlOrEmpty(this string url)
         {
-            if (string.IsNullOrEmpty(url))
-                return true;
 
-            return url is string valueAsString &&
-                   (valueAsString.StartsWith("http://", StringComparison.OrdinalIgnoreCase)
-                    || valueAsString.StartsWith("https://", StringComparison.OrdinalIgnoreCase)
-                    || valueAsString.StartsWith("ftp://", StringComparison.OrdinalIgnoreCase));
+            if (string.IsNullOrEmpty(url))
+            {
+                return true;
+            }
+
+            string[] validSchemes = { "http://", "https://", "ftp://" };
+            return validSchemes.Any(scheme => url.StartsWith(scheme, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
