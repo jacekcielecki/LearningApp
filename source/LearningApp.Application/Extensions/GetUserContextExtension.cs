b6b1b4bad1224e394c013dd69b082f5463aabffa
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-06-16 21:26:10 +0200

    Update GetUserContextExtension

---
+++ source/LearningApp.Application/Extensions/GetUserContextExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace LearningApp.Application.Extensions
+{
+    public static class GetUserContextExtension
+    {
+        public static int GetUserId(this HttpContext httpContext)
+        {
+            var userIdClaim = httpContext.User.Claims.SingleOrDefault(c => c.Type == "jti");
+            if (userIdClaim != null && int.TryParse(userIdClaim.Value, out int userId))
+            {
+                return userId;
+            }
+
+            throw new InvalidOperationException("User ID claim not found or invalid.");
+        }
+
+        public static ClaimsPrincipal GetUserContext(this HttpContext httpContext) => httpContext.User;
+
+    }
+}
