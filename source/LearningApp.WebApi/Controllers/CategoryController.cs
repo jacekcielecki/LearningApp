ce07c70b4e1fe6e511b52557b59730125e4c797a
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-06-19 16:16:46 +0200

    Add Category custom auth

--- source/LearningApp.WebApi/Controllers/CategoryController.cs
+++ source/LearningApp.WebApi/Controllers/CategoryController.cs
@@ -1,8 +1,8 @@
 ﻿using LearningApp.Application.Extensions;
 using LearningApp.Application.Interfaces;
 using LearningApp.Application.Requests.Category;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace LearningApp.WebApi.Controllers
 {
@@ -11,49 +11,46 @@ namespace LearningApp.WebApi.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
+        private readonly ClaimsPrincipal _userContext;
 
         public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService;
+            _userContext = HttpContext.GetUserContext();
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllAsync()
         {
-            var response = await _categoryService.GetAllAsync();
+            var response = await _categoryService.GetAllAsync(_userContext);
             return Ok(response);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
-            var response = await _categoryService.GetByIdAsync(id);
+            var response = await _categoryService.GetByIdAsync(id, _userContext);
             return Ok(response);
         }
 
         [HttpPost]
-        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> CreateAsync([FromBody] CreateCategoryRequest createCategoryRequest)
         {
-            var userId = HttpContext.GetUserId();
-            var response = await _categoryService.CreateAsync(createCategoryRequest, userId);
+            var response = await _categoryService.CreateAsync(createCategoryRequest, _userContext);
             return Ok(response);
         }
 
         [HttpPut("{id}")]
-        //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> UpdateAsync(int id, [FromBody] UpdateCategoryRequest updateCategoryRequest)
         {
-            var user = HttpContext.GetUserContext();
-            var response = await _categoryService.UpdateAsync(id, updateCategoryRequest, user);
+            var response = await _categoryService.UpdateAsync(id, updateCategoryRequest, _userContext);
             return Ok(response);
         }
 
         [HttpDelete("{id}")]
-        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> DeleteAsync(int id)
         {
-            await _categoryService.DeleteAsync(id);
+            await _categoryService.DeleteAsync(id, _userContext);
             return Ok();
         }
     }
