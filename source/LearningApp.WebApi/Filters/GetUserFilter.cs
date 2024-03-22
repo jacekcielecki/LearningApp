fd3b5c352796e51fecd7f3f9cf82fe186b75797c
Jacek Cielecki <jcielecki@sii.pl>
2024-03-22 12:58:00 +0100

    Add GetUserFilter

---
+++ source/LearningApp.WebApi/Filters/GetUserFilter.cs
@@ -0,0 +1,29 @@
+﻿using LearningApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LearningApp.WebApi.Filters;
+
+public class GetUserFilter : IAsyncActionFilter
+{
+    private readonly IUserService _userService;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public GetUserFilter(IUserService userService, IHttpContextAccessor httpContextAccessor)
+    {
+        _userService = userService;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var jtiClaim = _httpContextAccessor.HttpContext?.User.Claims.SingleOrDefault(c => c.Type == "jti");
+        var userId = Convert.ToInt32(jtiClaim?.Value);
+
+        var user = await _userService.GetByIdAsync(userId);
+
+        ArgumentNullException.ThrowIfNull(user, "User with given id not found");
+
+        context.ActionArguments["user"] = user;
+        await next();
+    }
+}
